e113f740c2d187b865bb33509a95fbf11bbaa5bd
9alexxxian4 <129763695+9alexxxian4@users.noreply.github.com>
2023-09-07 18:02:22 +0900

    17장
    반복문 사용하기 (for)

--- 17.js
+++ 17.js
@@ -1,7 +1,14 @@
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
-console.log('------------');
+
 for (let i = 9; i >= 0; i--) {
   console.log(i);
 }
+
+
+// for (초기화; 반복문 조건; 반복 중 처리 구문) {
+//   반복 처리 내용
+// }
+
+// 역순으로도 가능하다
