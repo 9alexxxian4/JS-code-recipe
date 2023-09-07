f77b8997a40b95ce2642ed902523f99367306110
alex <alexxxxian@naver.com>
2023-09-07 16:03:12 +0900

    18장
    반복문 사용하기(while)

---
+++ 18.js
@@ -0,0 +1,14 @@
+let myNumber = 0;
+
+while (myNumber < 10) {
+  console.log(myNumber);
+  myNumber += 1;
+}
+
+
+let myNumber2 = 0;
+
+while (myNumber2 < 10) {
+  myNumber2 += 1;
+  console.log(myNumber2);
+}
