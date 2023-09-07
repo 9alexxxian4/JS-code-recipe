9f54bf34724700a50240ba9b2ae02788d2c252b2
9alexxxian4 <129763695+9alexxxian4@users.noreply.github.com>
2023-09-07 18:15:20 +0900

    28장
    문자열의 공백 처리하기 (trim)

--- 28.js
+++ 28.js
@@ -9,3 +9,7 @@ console.log(trim2);
 const string3 = '    사과.  귤.   ';
 const trim3 = string3.trim();
 console.log(trim3);
+
+
+// 문자열.trim() - 문자열 양 끝의 공백을 제거 - 문자열반환
+// 공백은 스페이스, 탭, 줄바꿈 문자를 의미하며, 양단이 아닌 문자열 사이의 공백은 제거되지 않는다.
