17326b404ec2acab434119bb6e46111d3660d205
alex <alexxxxian@naver.com>
2023-09-07 17:34:08 +0900

    28장
    문자열의 공백 처리하기

---
+++ 28.js
@@ -0,0 +1,11 @@
+const string1 = '    안녕하세요    ';
+const trim1 = string1.trim();
+console.log(trim1);
+
+const string2 = '사과를 먹었어요\n';
+const trim2 = string2.trim();
+console.log(trim2);
+
+const string3 = '    사과.  귤.   ';
+const trim3 = string3.trim();
+console.log(trim3);
