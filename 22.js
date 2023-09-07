f297741c723e4bf03fb45691f5a0bc5f6e2d8299
9alexxxian4 <129763695+9alexxxian4@users.noreply.github.com>
2023-09-07 18:09:46 +0900

    22장
    어림 계산하기 (반올림,올림,버림)

--- 22.js
+++ 22.js
@@ -16,3 +16,9 @@ console.log(Math.floor(-8.6));
 console.log(Math.ceil(-8.6));
 
 console.log(Math.trunc(-8.6));
+
+
+// Math.round(값) - 반올림하기 - 숫자 반환
+// Math.floor(값) - 버림하기(값보다 작고 제일 가까운 정수 변환) - 숫자 반환
+// Math.ceil(값) - 올림하기(값보다 크고 제일 가까운 정수 변환) - 숫자 반환
+// Math.trunc(값) - 값의 정수 부분만을 반환 - 숫자 반환
