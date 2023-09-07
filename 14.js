22c54daa76790905a5c33c7edb1015c826674c84
alex <alexxxxian@naver.com>
2023-09-07 15:25:35 +0900

    14
    다수의 파라미터를 가지는 함수 정의하기

---
+++ 14.js
@@ -0,0 +1,13 @@
+function calcSum(...prices) {
+  let result = 0;
+  for (const value of prices) {
+    result += value;
+  }
+  return result;
+}
+
+const result1 = calcSum(1, 2, 3, 4, 5);
+console.log(result1);
+
+const result2 = calcSum(5, 10, 15);
+console.log(result2);
