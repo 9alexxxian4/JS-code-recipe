ef4700a7ca3dd7de280ed6f2d4431bbd1760489b
alex <alexxxxian@naver.com>
2023-09-08 14:00:51 +0900

    30장
    문자열 검색하기(부분 검색)

---
+++ 30.js
@@ -0,0 +1,13 @@
+const string = 'JavaScript를 배우자';
+
+const a1 = 'JavaScript를 배우자'.includes('JavaScript');
+console.log(a1);
+
+const a2 = 'JavaScript를 배우자'.startsWith('배우자');
+console.log(a2);
+
+const a3 = 'JavaScript를 배우자'.endsWith('배우자');
+console.log(a3);
+
+const a4 = 'JavaScript를 배우자'.includes('배우자',12);
+console.log(a4);
