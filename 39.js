6bd033a0887d3da1e70729f9624973e69813e322
9alexxxian4 <129763695+9alexxxian4@users.noreply.github.com>
2023-09-09 22:51:04 +0900

    39장
    정규 표현식 사용하기

--- 39.js
+++ 39.js
@@ -1,5 +1,7 @@
 if(/iPhone|iPod|iPad/.test(navigator.userAgent)) {
-  console.log('접속한 브라우저는 ios입니다.');
+  console.log('접속한 브라우저는 iOS입니다.');
 } else {
-  console.log('접속한 브라우저는 ios가 아닙니다.')
+  console.log('접속한 브라우저는 iOS가 아닙니다.')
 }
+
+// 정규 표현식을 사용하여 접속한 브라우저가 iOS인지 판별할 수 있다.
