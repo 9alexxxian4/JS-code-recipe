d29eb02c346261f83eb694139ab7f2dcde52cf6f
alex <alexxxxian@naver.com>
2023-09-12 19:08:13 +0900

    54장
    배열 요소 결합하여 문자열 만들기

---
+++ 54.js
@@ -0,0 +1,7 @@
+const array1 = [2, 4, 10];
+console.log(array1.join()); // '2,4,10' (문자열)
+
+const array2 = ["a", "b", "c"];
+console.log(array2.join("")); // 'abc' (문자열)
+
+// 배열.join([결합문자열]*) - 배열 요소를 결합해 문자열 만들기 - 문자열 반환
