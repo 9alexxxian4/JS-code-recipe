54fb7f9e1f490fed12f8ef1657d90ba1dadc2cad
alex <alexxxxian@naver.com>
2023-09-12 19:06:56 +0900

    48장
    배열 요소 다루기2

---
+++ 48.js
@@ -0,0 +1,8 @@
+const array = ["딸기", "귤", "사과"];
+
+//배열의 각 요소별 루프 처리
+for (const value of array) {
+  console.log(value); // '딸기','귤','사과'가 순서대로 출력
+}
+
+// for (const 요소 of 배열) {} - for...of 루프 처리
