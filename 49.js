f7214eb5443a865c81e6b8c807a32487340254ae
alex <alexxxxian@naver.com>
2023-09-12 19:07:11 +0900

    49장
    배열 요소 다루기3

---
+++ 49.js
@@ -0,0 +1,11 @@
+const array = ["딸기", "귤", "사과"];
+
+// 배열의 길이 가져오기
+const arrayLength = array.length;
+
+// 배열의 요소 데이터 처리
+for (let i = 0; i < arrayLength; i++) {
+  console.log(array[i]); // '딸기','귤','사과' 순서로 출력
+}
+
+// for (let i = 0; i < 배열길이; i++) {} - for문 루프 처리
