5db4b1927b5415a2897f6030c26f33bb09fad077
alex <alexxxxian@naver.com>
2023-09-13 15:41:58 +0900

    67장
    다양한 데이터 타입을 가진 객체 사용하기

---
+++ 67.js
@@ -0,0 +1,17 @@
+// 개인의 정보 데이터 객체
+const person = {
+  id: 1,
+  name: '거북이',
+  age: 28
+};
+// 속성 -> 'id:1','name:거북이','age:28'
+// 키 -> id, name, age
+// 값 -> 1, '거북이', 28
+
+console.log(person);
+// {
+//  "id": 1,
+//  "name": "거북이",
+//  "age": 28
+// }
+
