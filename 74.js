7c88481b264fe93bc483d1c53fbd62372da88685
alex <alexxxxian@naver.com>
2023-09-14 10:53:43 +0900

    74장
    데이터 타입 이해하기

---
+++ 74.js
@@ -0,0 +1,18 @@
+// 원시(Primitive) 타입 (기본) - 원형 데이터
+// 객체(Object) 타입 (복합) - 참조 데이터
+
+// 원시 타입
+const num = 100;
+const str = '사자';
+
+// 객체 타입
+const arr = [1,2,3];
+const obj = {
+  age: 18,
+  name: '사자'
+};
+const arr2 = [
+  { id: 10, name: '사자'},
+  { id: 20, name: '여우'},
+  { id: 30, name: '곰'},
+];
