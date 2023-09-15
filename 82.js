5275b928fa71e18e98bacfce6e78f559e80b7377
alex <alexxxxian@naver.com>
2023-09-15 15:45:49 +0900

    82장
    날짜 다루기(연도)

---
+++ 82.js
@@ -0,0 +1,8 @@
+const date = new Date();
+const year = date.getFullYear(); // 연도
+
+console.log(`지금은 ${year}년 ${date} 입니다.`);
+// "지금은 2023년 Fri Sep 15 2023 15:45:15 GMT+0900 (한국 표준시) 입니다."
+
+
+// getFullYear() - 연도 불러오기 - 숫자 반환
