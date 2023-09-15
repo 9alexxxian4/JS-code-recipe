e158d6a56e94a5675ae76416599461819136d835
alex <alexxxxian@naver.com>
2023-09-15 15:00:43 +0900

    76장
    데이터 타입 확인하기

---
+++ 76.js
@@ -0,0 +1,13 @@
+console.log(typeof true); // boolean
+console.log(typeof 10); // number
+console.log(typeof '사자'); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof Symbol()); // symbol
+console.log(typeof [1,2,3]); // object
+console.log(typeof { id:10, name: "호랑이"}); // object
+console.log(typeof function(){console.log('test');}); // function
+console.log(typeof class MyClass {}); // function
+
+
+// typeof 데이터 - 데이터 타입을 확인
