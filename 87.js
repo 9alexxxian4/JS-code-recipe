18b4eea3595f1e4828ad210ae35d1a6fd59a68a7
alex <alexxxxian@naver.com>
2023-09-16 11:51:19 +0900

    87장
    날짜 문자열의 타임스탬프 확인하기

---
+++ 87.js
@@ -0,0 +1,7 @@
+const num1 = Date.parse('2023/09/16');
+console.log(num1); // 1694790000000
+
+const num = Date.now();
+console.log(num); // 1694832623875
+
+// Date.parse() - 날짜 문자열 타임스탬프 가져오기 - 숫자 반환
