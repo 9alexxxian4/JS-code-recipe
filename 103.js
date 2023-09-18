5b03d06b3df1c5ca29521c30b7d5443c35abf35f
alex <alexxxxian@naver.com>
2023-09-18 16:51:29 +0900

    103장
    페이지 이동하기(앞/뒤로 가기)

---
+++ 103.js
@@ -0,0 +1,14 @@
+// 브라우저의 뒤로가기
+history.back();
+
+// 브라우저의 앞으로 가기
+history.forward();
+
+// 임의의 숫자를 전달하여 음수면 뒤로가기, 양수면 앞으로가기
+history.go(-1); // 뒤로 가기와 같음
+
+
+
+// history.back() - 뒤로 가기로 화면 이동
+// history.forward() - 앞으로 가기로 화면 이동
+// history.go() - 임의의 수만큼 화면 이동
